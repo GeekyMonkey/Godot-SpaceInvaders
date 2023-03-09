890257423a9376a2e666c6d9114c43ce6a7e88d4
Russ Painter <russp@geekymonkey.com>
2023-03-09 22:46:08 +0000

    Explosion light

--- Prefabs/Aliens/AlienExplosionPrefab.cs
+++ Prefabs/Aliens/AlienExplosionPrefab.cs
@@ -10,13 +10,16 @@ public partial class AlienExplosionPrefab : Node2D
 
     private Sprite2D Sprite;
     private AudioStreamPlayer2D ExplosionSound;
-
+    private PointLight2D ExplosionLight;
+    private float LightEnergy;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         Sprite = GetNode<Sprite2D>("./AlienDeathSprite");
         ExplosionSound = GetNode<AudioStreamPlayer2D>("./ExplosionSound");
+        ExplosionLight = GetNode<PointLight2D>("./ExplosionLight");
+        LightEnergy = ExplosionLight.Energy;
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,10 +27,11 @@ public partial class AlienExplosionPrefab : Node2D
     {
         Age += delta;
         float opacity = Math.Clamp(1f - (float)(Age / FadeSeconds), 0, 1f);
-        Sprite.Modulate = new Color(1f, 1f, 1f, opacity);
+        Sprite.Modulate = new Color(Sprite.Modulate.R, Sprite.Modulate.G, Sprite.Modulate.B, opacity);
         if (opacity <= 0 && !ExplosionSound.Playing)
         {
             QueueFree();
         }
+        ExplosionLight.Energy = LightEnergy * opacity;
     }
 }
