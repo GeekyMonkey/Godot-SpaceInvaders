fed0bd56a02cb19f590bc89b07a90519eb8138d8
Russ Painter <russp@geekymonkey.com>
2023-03-18 13:57:13 +0000

    Glow

---
+++ SpaceInvaders_GD/Prefabs/Bullet/BulletSprite.gd
@@ -0,0 +1,40 @@
+class_name BulletSprite
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+"""
+// Editor State
+[Export] public Color Color = Color.FromHsv(0.5f, 0.8f, 0, 8f);
+[Export] public float Speed = .1f;
+
+// Private state
+private float Hue;
+private float Sat;
+private float Val;
+
+/// <summary>
+/// Called when the node enters the scene tree for the first time.
+/// </summary>
+public override void _Ready()
+{
+Color.ToHsv(out Hue, out Sat, out Val);
+}
+
+/// <summary>
+/// Rotate the color
+/// </summary>
+public override void _Process(double delta)
+{
+Hue += (float)delta * Speed;
+this.Modulate = Color.FromHsv(Hue, Sat, Val);
+}
+"""
